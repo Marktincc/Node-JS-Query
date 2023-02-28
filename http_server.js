57e30dfef828789d5d337bbff4084bda304ab188
Marktincc <105111683+Marktincc@users.noreply.github.com>
2023-02-27 23:08:37 -0500

    Update http_server.js

--- http_server.js
+++ http_server.js
@@ -3,7 +3,7 @@ const datetime =require("./date.js");
 
 http.createServer(function(req,res){
     res.writeHead(200,{'Content-Type':'text/html'});
-    res.end(datetime.myDateTime());
+    res.end(`La hora es: ${datetime.myDateTime()}`);
 }).listen(8080);
 
 // npm init -yes para que sirva el http
